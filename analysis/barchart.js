1e860f471dc03972e6dd41fdde1c8714a4a41377
Gabriel Wu <gabrielwu84@gmail.com>
2017-08-02 01:27:42 +0800

    first split - problem with load sequence, - need to put callback, figure out where.

--- analysis/barchart.js
+++ analysis/barchart.js
@@ -1,14 +1,64 @@
 define(["d3js"],function(d3){
-
 	return function(params){
-		
+		var opt=params.options;
+		var d3Obj, label, vAxis, dataBar;
+		var yScale, barScale;
 
-		
+		var axisStrokeWidth=3;
+		var axisSpace=2, barSpace=5; //in percentage
+		var pHeight=500, pWidth=600, barColor='#7755ff';
+		var barOffset=150;
+
+		var data=new Array(opt.length).fill(0);
+		var respDom=document.createElement("div");
+
+		require(["d3js"],function(d3) {
+			yScale=d3.scaleLinear()
+				.domain([-0.5,opt.length-0.5])
+				.range([0,pHeight]);
+			barScale=d3.scaleLinear()
+				.domain([0,20])
+				.range([0,pWidth-barOffset])
+			d3Obj=d3.select(respDom).append("svg")
+				.attr('height',pHeight).attr('width',pWidth)
+			label=d3Obj.selectAll("text.graphLabels").data(opt).enter().append("text")
+				.attr("class","graphLabels")
+			vAxis=d3Obj.selectAll("line.axisLine").data(opt).enter().append("line")
+				.attr("class","axisLine")
+			dataBar=d3Obj.selectAll("rect").data(opt).enter().append('rect')
+				.style('fill',barColor)
+				.attr('x',barOffset+axisStrokeWidth/2)
+				.attr('y',function(d,i){
+					return yScale(-0.5+i+0.01*barSpace);
+				})
+				.attr('height',yScale(0.5-0.02*barSpace))
+			label.data(opt)
+				.attr("dominant-baseline", "central").attr("text-anchor","end")
+				.attr("dx","-10").attr("font-size",15)
+				.attr("x",barOffset)
+				.attr("y",function(d,i){return yScale(i);})
+				.html(function(d){return d;})
+			vAxis.data(opt)
+				.attr("x1",barOffset).attr("x2",barOffset)
+				.attr("stroke-width",axisStrokeWidth).attr("stroke","#333")
+				.attr("y1",function(d,i){
+					return yScale(i-0.5+0.01*axisSpace);
+				})
+				.attr("y2",function(d,i){
+					return yScale(i+0.5-0.01*axisSpace);
+			 	})
+		});
+
 		this.dom=function(){
-
+			return respDom;
 		}
 
-		this.update=function(data){
+		this.update=function(newData){
+			dataBar
+				.data(newData)
+				.attr('width',function(d,i){
+					return barScale(d);
+				})
 
 		}
 	}
--- mods/mcq.js
+++ mods/mcq.js
@@ -1,6 +1,4 @@
 define(["ctype"], function(ctype){
-
-
 	return {
 		authEngine:function(){
 			var editDiv=document.createElement("div");
@@ -227,65 +225,71 @@ define(["ctype"], function(ctype){
 		},
 		webEngine:function(params){
 			var webObj=this;
-			var opt=params.options;
-			var d3Obj, label, vAxis, dataBar;
-			var yScale, barScale;
+			var analysisObj;
 
-			var axisStrokeWidth=3;
-			var axisSpace=2, barSpace=5; //in percentage
-			var pHeight=500, pWidth=600, barColor='#7755ff';
-			var barOffset=150;
+			require(["http://gabrielwu84.dlinkddns.com/clicker-prod/analysis/barchart.js"],function(barchart){
+				analysisObj=new barchart(params);
+			})
 
-			var data=new Array(opt.length).fill(0);
-			var respDom=document.createElement("div");
+			// var opt=params.options;
+			// var d3Obj, label, vAxis, dataBar;
+			// var yScale, barScale;
 
-			require(["d3js"],function(d3) {
+			// var axisStrokeWidth=3;
+			// var axisSpace=2, barSpace=5; //in percentage
+			// var pHeight=500, pWidth=600, barColor='#7755ff';
+			// var barOffset=150;
 
-				yScale=d3.scaleLinear()
-					.domain([-0.5,opt.length-0.5])
-					.range([0,pHeight]);
-				barScale=d3.scaleLinear()
-					.domain([0,20])
-					.range([0,pWidth-barOffset])
-				d3Obj=d3.select(respDom).append("svg")
-					.attr('height',pHeight).attr('width',pWidth)
-				label=d3Obj.selectAll("text.graphLabels").data(opt).enter().append("text")
-					.attr("class","graphLabels")
-				vAxis=d3Obj.selectAll("line.axisLine").data(opt).enter().append("line")
-					.attr("class","axisLine")
-				dataBar=d3Obj.selectAll("rect").data(opt).enter().append('rect')
-					.style('fill',barColor)
-					.attr('x',barOffset+axisStrokeWidth/2)
-					.attr('y',function(d,i){
-						return yScale(-0.5+i+0.01*barSpace);
-					})
-					.attr('height',yScale(0.5-0.02*barSpace))
-				label.data(opt)
-					.attr("dominant-baseline", "central").attr("text-anchor","end")
-					.attr("dx","-10").attr("font-size",15)
-					.attr("x",barOffset)
-					.attr("y",function(d,i){return yScale(i);})
-					.html(function(d){return d;})
+			// var data=new Array(opt.length).fill(0);
+			// var respDom=document.createElement("div");
 
-					//MathJax.Hub.Typeset(x)
-				vAxis.data(opt)
-					.attr("x1",barOffset).attr("x2",barOffset)
-					.attr("stroke-width",axisStrokeWidth).attr("stroke","#333")
-					.attr("y1",function(d,i){
-						return yScale(i-0.5+0.01*axisSpace);
-					})
-					.attr("y2",function(d,i){
-						return yScale(i+0.5-0.01*axisSpace);
-				 	})
-			});
+			// require(["d3js"],function(d3) {
 
-			function update(newData){
-				dataBar.data(newData)
-					.attr('width',function(d,i){
-						return barScale(d);
-					})
-			}
-			var appObj; this.widBody=document.createElement("div");
+			// 	yScale=d3.scaleLinear()
+			// 		.domain([-0.5,opt.length-0.5])
+			// 		.range([0,pHeight]);
+			// 	barScale=d3.scaleLinear()
+			// 		.domain([0,20])
+			// 		.range([0,pWidth-barOffset])
+			// 	d3Obj=d3.select(respDom).append("svg")
+			// 		.attr('height',pHeight).attr('width',pWidth)
+			// 	label=d3Obj.selectAll("text.graphLabels").data(opt).enter().append("text")
+			// 		.attr("class","graphLabels")
+			// 	vAxis=d3Obj.selectAll("line.axisLine").data(opt).enter().append("line")
+			// 		.attr("class","axisLine")
+			// 	dataBar=d3Obj.selectAll("rect").data(opt).enter().append('rect')
+			// 		.style('fill',barColor)
+			// 		.attr('x',barOffset+axisStrokeWidth/2)
+			// 		.attr('y',function(d,i){
+			// 			return yScale(-0.5+i+0.01*barSpace);
+			// 		})
+			// 		.attr('height',yScale(0.5-0.02*barSpace))
+			// 	label.data(opt)
+			// 		.attr("dominant-baseline", "central").attr("text-anchor","end")
+			// 		.attr("dx","-10").attr("font-size",15)
+			// 		.attr("x",barOffset)
+			// 		.attr("y",function(d,i){return yScale(i);})
+			// 		.html(function(d){return d;})
+
+			// 		//MathJax.Hub.Typeset(x)
+			// 	vAxis.data(opt)
+			// 		.attr("x1",barOffset).attr("x2",barOffset)
+			// 		.attr("stroke-width",axisStrokeWidth).attr("stroke","#333")
+			// 		.attr("y1",function(d,i){
+			// 			return yScale(i-0.5+0.01*axisSpace);
+			// 		})
+			// 		.attr("y2",function(d,i){
+			// 			return yScale(i+0.5-0.01*axisSpace);
+			// 	 	})
+			// });
+
+			// function update(newData){
+			// 	dataBar.data(newData)
+			// 		.attr('width',function(d,i){
+			// 			return barScale(d);
+			// 		})
+			// }
+			var widletObj; this.widBody=document.createElement("div");
 			require(["vue"],function(Vue){
 				var opt={
 					template:'<div ref="content"></div>',
@@ -295,7 +299,7 @@ define(["ctype"], function(ctype){
 						optContent.putInto(this.$refs.content);
 					}
 				}
-				appObj=new Vue({
+				widletObj=new Vue({
 					el:webObj.widBody,
 					template:'\
 					<div class="form-group btn-group-vertical btn-block justify-content-end">\
@@ -339,12 +343,17 @@ define(["ctype"], function(ctype){
 			}
 
 			this.responseDom=function(){
-				return respDom;
+				// return respDom;
+				return analysisObj.dom();
 			}
 			this.processResponse=function(studentUuid,ans){
 				data[ans]++;
-				console.log(data, ans);
-				update(data);
+				// console.log(data, ans);
+				// update(data);
+				return analysisObj.update(data);
+			}
+			this.updateRespDim=function(height,width){
+				analysisObj.updateDim(height,width);
 			}
 		}
 	}
